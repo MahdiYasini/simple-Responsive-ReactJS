4862abd43a44cae929a3b41840f1092b99b692b0
M.Yasini <yasinimahdi98@gmail.com>
2020-03-24 11:00:23 +0430

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      </div>
+    </BrowserRouter>
   );
 }
 
